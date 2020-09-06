b98fac141fe99cffc06fd62d9e14051e323d5dfc
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-09-06 16:21:46 -0700

    validation functions, and continuiation prompt done

--- develop/main.js
+++ develop/main.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
+const format = require('asciiart-logo');
+const { isMainThread } = require('worker_threads');
 
 
 //establish connection to database
@@ -11,3 +13,40 @@ var connection = mysql.Connection({
     password: 'Costanza26.',
     database: 'employee_management_systemdb'
 });
+
+
+//make sure string answer is not a number
+function validateString(answer) {
+    if (answer != '' && isNaN(pareseInt(answer))) {
+        return true;
+    }
+    return false;
+}
+
+//make sure a integer is not a string 
+function validateInteger(answer) {
+    if (answer != '' && !isNaN(parseInt(answer))) { 
+        return true;
+    }
+        return false;
+}
+
+
+//prompt ran end of main prompts
+function continuePrompt() {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'continue',
+            message: 'Would you like to continue?'
+        }
+    ]).then(function (data) {
+        if (data.continue) {
+            mainPrompt();
+        }
+        else {
+            return;
+        }
+    })
+}
+
