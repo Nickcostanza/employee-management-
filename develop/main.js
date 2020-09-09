3990f5381b17c214e95da5b99ae6b95f3c50f6ee
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-09-08 17:20:24 -0700

    some bug fixes in schema, and syntax errors in main.js

--- develop/main.js
+++ develop/main.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const format = require('asciiart-logo');
-const { isMainThread } = require('worker_threads');
+//const { isMainThread } = require('worker_threads');
 
 
 //establish connection to database
-var connection = mysql.Connection({
+var connection = mysql.createConnection({
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
@@ -17,7 +17,7 @@ var connection = mysql.Connection({
 
 //make sure string answer is not a number
 function validateString(answer) {
-    if (answer != '' && isNaN(pareseInt(answer))) {
+    if (answer != '' && isNaN(parseInt(answer))) {
         return true;
     }
     return false;
@@ -167,18 +167,99 @@ function mainPrompt() {
                     {
                         type: 'input',
                         name: 'department',
-                        message: 'Enter the New Departmens Name'
+                        message: 'Enter the New Departments Name',
                         validate: validateString
                     }
                 ]).then(function(data) {
-                    var query = connection.query(`INSERT INTO department (department) VALUES ('${data.department}');`, funtion(err,data) {
+                    var query = connection.query(`INSERT INTO department (department) VALUES ('${data.department}');`, function(err,data) {
                         if (err) throw err;
                         return data;
                     })
                     console.log('Department Has Been Added')
+                    continuePrompt();
+                });
+                break;
+            //Update current employee
+            case 'Update Employees Role':
+                const emp = {
+                    first_name: "",
+                    last_name: "",
+                    role_id: 0,
+                    manager_id: 0,
+                    empId: 0
+                };
+                var query = connection.query('SELECT id, first_name, last_name FROM employee', function(err, data) {
+                    if (err) throw err;
+                    let choices = data.map(x => `${x.id} - $${x.first_name} ${x.last_name}`);
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'employee',
+                            message: 'Select an Employee',
+                            choices: [...choices]
+                        }
+                    ]).then(function(err, data) {
+                        var arr = data.employee.split('');
+                        emp.empID = parseInt(arr[0]);
+                        inquirer.prompt([
+                            {
+                               type: 'input',
+                               name: 'firstName', 
+                               message: 'Enter Employees First Name:',
+                               validate: validateString()
+
+                            }
+                        ]).then(function(data) {
+                            emp.first_name = data.firstName;
+                            emp.last_name = data.lastName;
+                            var query = connection.query('SELECT id, title FROM role', function (err, data) {
+                                if (err) throw err;
+                                let choices = data.map(x => `${x.id} - ${x.title}`);
+                                inquirer.prompt([
+                                    {
+                                        type: 'list',
+                                        name: 'title',
+                                        message: 'Select a Title:',
+                                        choices: [...choices]
+                                    }
+                                ]).then(function(data) {
+                                    var arr = data.title.split('');
+                                    emp.role_id = parseInt(arr[0]);
+                                    var query = connection.query('SELECT id, first_name, last_name FROM employee', function(err, data) {
+                                        if (err) throw err;
+                                        let choices = data.map(x => `${x.id} - ${x.first_name}`);
+                                        choices.push('This employee does not have a Manager');
+                                        inquirer.prompt([
+                                            {
+                                                type: 'list',
+                                                name: 'manager',
+                                                message: 'Select this Employees Manager:',
+                                                choices: [...choices]
+                                            }
+                                        ]).then(function(data) {
+                                            if (data.manager === 'This employee does not have a Manager') {
+                                                emp.manager_id = null;
+                                            }
+                                            else {
+                                                var arr = data.manager.split('');
+                                                emp.manager_id = parseInt(arr[0]);
+                                            }
+                                            var query = connection.query(`UPDATE employee SET first_name = '${emp.first_name}', last_name = '${emp.last_name}', role_id = ${emp.role_id}, manager_id = ${emp.manager_id} WHERE id = ${emp.empID}`, function (err, data) {
+                                                if (err) throw err;
+                                                continuePrompt();
+                                                return data;
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
                 });
                 break;
         }
-    })
+    });
 }
+
+mainPrompt();
 
--- develop/schema.sql
+++ develop/schema.sql
@@ -5,7 +5,7 @@ CREATE database employee_management_systemDB;
 USE employee_management_systemDB;
 
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INTEGER NOT NULL AUTO_INCREMENT,
     department VARCHAR(30) NOT NULL,
     PRIMARY KEY(ID)
@@ -16,7 +16,8 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
     department_id INTEGER NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employee (
@@ -25,9 +26,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER NULL,
-        PRIMARY KEY(id)
+        PRIMARY KEY(id),
+        FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-SELECT * FROM department;
-SELECT * FROM role;
-SELECT * FROM employee;
